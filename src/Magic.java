fc1de8a4ccb4bcfb704cd5c69b4b9ca8dee9fdd8
Daniela Moles <31218018+Dani-ms@users.noreply.github.com>
2023-06-09 15:26:46 +0100

    Add files via upload
    create first code in JAVA

---
+++ src/Magic.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Magic {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        int myNumber = 7 ; // number to document this
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6 ;
+        System.out.println(stepSix);
+    }
+}
